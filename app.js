78bcd378c916e3f9b8b99280f0f2d418eff994ea
DuongKien <ddk0705@gmail.com>
2018-02-03 16:27:14 +0700

    Create root file for Origami

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ var app = express();
 // Information config
 var rootFolder = config.get('rootFolder');
 var rootFile = config.get('rootFile');
+var rootFileOrigami = config.get('rootFileOrigami');
 var host = config.get('server.host');
 var port = config.get('server.port');
 
 app.use(express.static(__dirname + rootFolder));
-app.get('/*', function(req, res) {
+
+/**
+ * Default
+ */
+app.get('/', function(req, res) {
   res.sendFile(rootFile);
+});
+
+/**
+ * With user_id & app_id
+ */
+app.get('/:user/:app', function(req, res) {
+  
+  let userId = parseInt(req.params.user);
+  let appId = parseInt(req.params.app);
+
+  res.sendFile(rootFileOrigami + '/' + userId + '/' + appId + '/index.html');
+
 });
 
 var server = app.listen(process.env.PORT || port, host, function(){
